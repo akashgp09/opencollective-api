39076cb8138c3c83c718c911da6050bd504cc920
Nitin Kumar <snitin315@gmail.com>
2021-05-09 15:57:54 +0530

    feat(GQLV2): add editMember mutations

--- server/graphql/v2/mutation/MemberMutations.js
+++ server/graphql/v2/mutation/MemberMutations.js
@@ -7,9 +7,9 @@ import { editPublicMessage } from '../../common/members';
 import { Forbidden, Unauthorized } from '../../errors';
 import { MemberRole } from '../enum';
 import { AccountReferenceInput, fetchAccountWithReference } from '../input/AccountReferenceInput';
-import { MemberInvitationInput } from '../input/MemberInvitationInput';
 import { Member } from '../object/Member';
 import { MemberInvitation } from '../object/MemberInvitation';
+import ISODateTime from '../scalar/ISODateTime';
 
 const memberMutations = {
   editPublicMessage: {
@@ -51,9 +51,23 @@ const memberMutations = {
     type: new GraphQLNonNull(MemberInvitation),
     description: 'Invite a new member to the Collective',
     args: {
-      memberInvitation: {
-        type: GraphQLNonNull(MemberInvitationInput),
+      memberAccount: {
+        type: GraphQLNonNull(AccountReferenceInput),
         description: 'Reference to an account for the invitee',
+      },
+      account: {
+        type: GraphQLNonNull(AccountReferenceInput),
+        description: 'Reference to an account for the inviting Collective',
+      },
+      role: {
+        type: MemberRole,
+        description: 'Role of the invitee',
+      },
+      description: {
+        type: GraphQLString,
+      },
+      since: {
+        type: ISODateTime,
       },
     },
     async resolve(_, args, req) {
@@ -61,7 +75,7 @@ const memberMutations = {
         throw new Unauthorized('You need to be logged in to invite a member.');
       }
 
-      let { memberAccount, account } = args.memberInvitation;
+      let { memberAccount, account } = args;
 
       memberAccount = await fetchAccountWithReference(memberAccount);
       account = await fetchAccountWithReference(account);
@@ -70,8 +84,12 @@ const memberMutations = {
         throw new Unauthorized('Only admins can send an invitation.');
       }
 
+      if (![MemberRoles.ACCOUNTANT, MemberRoles.ADMIN, MemberRoles.MEMBER].includes(args.role)) {
+        throw new Forbidden('You can only invite accountants, admins, or members.');
+      }
+
       const memberParams = {
-        ...pick(args.memberInvitation, ['role', 'description', 'since']),
+        ...pick(args, ['role', 'description', 'since']),
         MemberCollectiveId: memberAccount.id,
         CreatedByUserId: req.remoteUser.id,
       };
@@ -87,6 +105,80 @@ const memberMutations = {
       });
 
       return invitation;
+    },
+  },
+  editMember: {
+    type: new GraphQLNonNull(Member),
+    description: 'Invite a new member to the Collective',
+    args: {
+      memberAccount: {
+        type: GraphQLNonNull(AccountReferenceInput),
+        description: 'Reference to an account for the donating Collective',
+      },
+      account: {
+        type: GraphQLNonNull(AccountReferenceInput),
+        description: 'Reference to an account for the receiving Collective',
+      },
+      role: {
+        type: MemberRole,
+        description: 'Role of member',
+      },
+      description: {
+        type: GraphQLString,
+      },
+      since: {
+        type: ISODateTime,
+      },
+    },
+    async resolve(_, args, req) {
+      if (!req.remoteUser) {
+        throw new Unauthorized('You need to be logged in to invite a member.');
+      }
+
+      let { memberAccount, account } = args;
+
+      memberAccount = await fetchAccountWithReference(memberAccount);
+      account = await fetchAccountWithReference(account);
+
+      if (!req.remoteUser.isAdminOfCollective(account)) {
+        throw new Unauthorized('Only admins can edit members.');
+      }
+
+      if (![MemberRoles.ACCOUNTANT, MemberRoles.ADMIN, MemberRoles.MEMBER].includes(args.role)) {
+        throw new Forbidden('You can only edit accountants, admins, or members.');
+      }
+
+      if (args.role === MemberRoles.ADMIN) {
+        const admins = await models.Member.findAll({
+          where: {
+            CollectiveId: account.id,
+            role: MemberRoles.ADMIN,
+          },
+        });
+
+        if (admins.length === 1) {
+          throw new Forbidden('There must be at least one admin for the account.');
+        }
+      }
+
+      // Edit member
+      const editableAttributes = pick(args, ['role', 'description', 'since']);
+
+      await models.Member.update(editableAttributes, {
+        where: {
+          id: memberAccount.id,
+          CollectiveId: account.id,
+        },
+      });
+
+      const member = await models.Member.findOne({
+        where: {
+          CollectiveId: account.id,
+          MemberCollectiveId: memberAccount.MemberCollectiveId,
+        },
+      });
+
+      return member;
     },
   },
   removeMember: {
@@ -122,6 +214,19 @@ const memberMutations = {
 
       if (![MemberRoles.ACCOUNTANT, MemberRoles.ADMIN, MemberRoles.MEMBER].includes(args.role)) {
         throw new Forbidden('You can only remove accountants, admins, or members.');
+      }
+
+      if (args.role === MemberRoles.ADMIN) {
+        const admins = await models.Member.findAll({
+          where: {
+            CollectiveId: account.id,
+            role: MemberRoles.ADMIN,
+          },
+        });
+
+        if (admins.length === 1) {
+          throw new Forbidden('There must be at least one admin for the account.');
+        }
       }
 
       // Remove member
